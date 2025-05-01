c03bef625127a92a1b4172381cf62597fdf1a4d3
Rushik145 <rushikdobariya8212@gmail.com>
2025-05-01 20:46:06 +0530

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   const testimonials = [
     {
       quote:
-        "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple",
+        "It was nice working with chirag significantly improved our team's productivity. The intuitive interface makes complex tasks simple",
       name: "Sachin Gondaliya",
       designation: "Freelancing Client",
       src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
